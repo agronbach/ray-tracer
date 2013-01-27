2ce4cf8ca6c9eec20f9635869fead4c3de0fbc64
Christopher Hebert <hebert.christopherj@gmail.com>
2013-01-26 18:25:04 -0800

    added shadows

--- main.cc
+++ main.cc
@@ -98,13 +98,15 @@ bool does_ray_collide_with_sphere(const Ray& ray, Sphere* sphere, float t0, floa
 // Returns true if the ray hits any of |g_spheres| in the range t [0.0f, T_MAX]
 // If true, populates |hit_sphere_index| with the index into |g_spheres| of the
 // colliding sphere and |collision_t| with the 't' value of the collision.
-bool test_for_hit(const Ray& ray, int *hit_sphere_index, float *collision_t) {
+bool test_for_hit(const Ray& ray, int *hit_sphere_index, float *collision_t, int ignore_sphere_index) {
    float t0 = 0.0f;
    float t1 = T_MAX;
    bool hit = false;
 
    int i;
    for (i = 0; i < g_num_spheres; ++i) {
+      if (i == ignore_sphere_index)
+         continue;
       if (does_ray_collide_with_sphere(ray, &g_spheres[i], t0, t1, collision_t)) {
          hit = true;
          *hit_sphere_index = i;
@@ -122,22 +124,45 @@ void add_sphere_to_scene(const vec3& center, float radius, Material material) {
    g_spheres[g_num_spheres++].radius = radius;
 }
 
-vec3 reflection(vec3* light_direction, vec3* normal) {
-   return normalize(*normal * 2.0f*(dot(*light_direction, *normal)) - *light_direction);
+vec3 reflection(const vec3& light_direction, const vec3& normal) {
+   return normalize(normal * 2.0f*(dot(light_direction, normal)) - light_direction);
 }
 
-vec3 pixel_color(Sphere* sphere, Light* light, vec3* light_direction, vec3* normal, vec3* view_direction) {
+Ray make_shadow_feeler_ray(const vec3& position, const vec3& light_position) {
+   Ray ray;
+   ray.origin = position;
+   ray.direction = light_position - position;
+   return ray;
+}
+
+vec3 pixel_color(int sphere_index, const Light& light, const vec3& light_direction, const vec3& position, const vec3& view_direction) {
    vec3 color;
-   float reflection_component = dot(reflection(light_direction, normal), *view_direction);
-   float diffuse_component = dot(*light_direction, *normal);
+   vec3 normal = normalize(position - g_spheres[sphere_index].center);
+
+   bool is_shadow = false;
+   Ray shadow_feeler_ray(make_shadow_feeler_ray(position, light.position));
+   int hit_sphere_index;
+   float collision_t;
+   if (test_for_hit(shadow_feeler_ray, &hit_sphere_index, &collision_t, sphere_index)) {
+      is_shadow = true;
+   }
+
+   float reflection_component;
+   float diffuse_component = dot(light_direction, normal);
+   if (!is_shadow) {
+      reflection_component = dot(reflection(light_direction, normal), view_direction);
+      diffuse_component = dot(light_direction, normal);
+   }
+
+
    for (int i = 0; i < 3; ++i) {
       float diffuse = 0.0f;
       float specular = 0.0f;
-      if (reflection_component > 0.0f)
-         specular = light->material.specular[i]*sphere->material.specular[i] * pow(reflection_component, sphere->material.alpha);
-      if (diffuse_component > 0.0f)
-         diffuse = light->material.diffuse[i]*sphere->material.diffuse[i] * diffuse_component;
-      float ambient = light->material.ambient[i]*sphere->material.ambient[i];
+      if (!is_shadow && reflection_component > 0.0f)
+         specular = light.material.specular[i]*g_spheres[sphere_index].material.specular[i] * pow(reflection_component, g_spheres[sphere_index].material.alpha);
+      if (!is_shadow && diffuse_component > 0.0f)
+         diffuse = light.material.diffuse[i]*g_spheres[sphere_index].material.diffuse[i] * diffuse_component;
+      float ambient = light.material.ambient[i]*g_spheres[sphere_index].material.ambient[i];
 
       color[i] = ambient + diffuse + specular;
    }
@@ -149,7 +174,7 @@ Light makeReddishLight() {
    light.material.diffuse = vec3(1);
    light.material.specular = vec3(.8);
    light.material.ambient = vec3(.8);
-   light.position = vec3(10, 15, 0);
+   light.position = vec3(0, 5, 10);
    return light;
 }
 
@@ -172,7 +197,7 @@ Material makeDiffuseMaterial() {
    Material mat;
    mat.diffuse = vec3(.7, 0, 0);
    mat.specular = vec3(0, 0, 0.8);
-   mat.ambient = vec3(0, .1, 0);
+   mat.ambient = vec3(0, .2, 0);
    mat.alpha = 20.0f;
    return mat;
 }
@@ -220,14 +245,13 @@ void calculate_pixel_buffer_from_ray_trace(Camera& camera, Light& light) {
          Ray ray(ray_from_pixel_coordinates(row, col, camera));
          int hit_sphere_index;
          float collision_t;
-         if (test_for_hit(ray, &hit_sphere_index, &collision_t)) {
+         if (test_for_hit(ray, &hit_sphere_index, &collision_t, -1)) {
             // find first object hit by ray and its surface normal n
             vec3 position = ray.origin + collision_t*normalize(ray.direction);
-            vec3 normal = normalize(position - g_spheres[hit_sphere_index].center);
             vec3 light_direction = normalize(light.position - position);
             vec3 view_direction = normalize(camera.position - position);
             // set pixel color to value based on material, light, and n
-            g_pixel_buffer[row][col] = pixel_color(&g_spheres[hit_sphere_index], &light, &light_direction, &normal, &view_direction);
+            g_pixel_buffer[row][col] = pixel_color(hit_sphere_index, light, light_direction, position, view_direction);
          } else {
             // If no hit occurs, set to the background color
             g_pixel_buffer[row][col] = kBackgroundColor;
@@ -240,7 +264,7 @@ int main(int argc, char** argv) {
    Camera camera = makeDefaultCamera();
 
    add_sphere_to_scene(vec3(.5, 0, 25), 0.6f, makeDiffuseMaterial());
-   add_sphere_to_scene(vec3(0, .5, 25), 0.3f, makeSpecularMaterial());
+   add_sphere_to_scene(vec3(0, .5, 23), 0.1f, makeSpecularMaterial());
    add_sphere_to_scene(vec3(-.5, -.5, 25), 0.4f, makeAmbientMaterial());
 
    Light light = makeReddishLight();
